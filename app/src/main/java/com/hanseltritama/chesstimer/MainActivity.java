94d80a6b8ec846ccb4bea42419b7a6bf029bb216
Hansel Tritama <hanseltritama@LMHANSELT05283.local>
2018-11-29 18:12:58 -0800

    added min and sec formatter

--- app/src/main/java/com/hanseltritama/chesstimer/MainActivity.java
+++ app/src/main/java/com/hanseltritama/chesstimer/MainActivity.java
@@ -1,7 +1,13 @@
 package com.hanseltritama.chesstimer;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView textView = findViewById(R.id.textView);
+        TextView textView2 = findViewById(R.id.textView2);
+
+        CountDownTimer countDownTimer = new CountDownTimer(60000, 1000) {
+
+            public void onTick(long millisecondsUntilDone) {
+                NumberFormat f = new DecimalFormat("00");
+                long min = (millisecondsUntilDone/60000) % 60;
+                long sec = (millisecondsUntilDone/1000) % 60;
+                textView.setText(f.format(min) + ":" + f.format(sec));
+            }
+
+            public void onFinish() {
+                Log.i("Done!", "Countdown is finished!");
+            }
+
+        }.start();
+
+        countDownTimer.start();
     }
 }
